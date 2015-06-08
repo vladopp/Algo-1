09aeda52d58bd68828570462e3a0395ee316fcaa
Vladimir Petkov <vlado@itgbg.com>
2015-06-08 14:46:05 +0300

    adding vector implementation

---
+++ week1/1-Vector/vector.cpp
@@ -0,0 +1,105 @@
+class Vector
+{
+public:
+	Vector();
+	Vector(const Vector& other);
+	~Vector();
+	Vector& operator =(const Vector& other);
+	void insert(int index, double value);
+	void add(double value);
+	double get(int index);
+	int size();
+	int capacity();
+private:
+	double* arr;
+	int size;
+	int capacity;
+};
+
+
+Vector::Vector()
+{	
+	arr = new double[512];
+	size = 0;
+	capacity = 512;
+}
+
+
+Vector::~Vector()
+{
+	size = 0;
+	capacity = 0;
+	delete[] arr;
+	arr=0;
+}
+
+
+Vector::Vector(const Vector& other)
+{
+	capcity = other.capacity;
+	size = other.size;
+	arr = new double[capacity];
+	for(int i=0; i<size; i++)
+	{
+		arr[i] = other.arr[i];
+	}
+}
+
+
+Vector& Vector::operator =(const Vector& other)
+{
+	capcity = other.capacity;
+	size = other.size;
+	arr = new double[capacity];
+	for(int i=0; i<size; i++)
+	{
+		arr[i] = other.arr[i];
+	}
+	return *this;
+}
+
+
+void Vector::insert(int index, double value)
+{
+	if(size < index && index < capacity)
+	{
+		arr[index] = value;
+	}
+	else if(size != capacity)
+	{
+		for(int i=size; i!=index-1; i--)
+		{
+			arr[i+1] = arr[i];
+		}
+		arr[index] = valuel
+	}
+// another case to consider
+}
+
+
+void add(double value)
+{
+	if(size != capacity)
+	{
+		arr[size] = value;
+		size++;
+	}
+}
+
+double get(int index)
+{
+	return arr[index];
+}
+
+
+int size()
+{
+	return size;
+}
+
+
+int capacity()
+{
+	return capacity;
+}
+
